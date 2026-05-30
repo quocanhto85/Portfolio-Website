59a327967fb543c2977d78215a87dde70813ff5e
Quoc Anh <quocanhto85@gmail.com>
2026-05-30 10:57:54 +0930

    Setting up Vercel Spped Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Alfred from "./components/Alfred";
 
@@ -35,6 +36,7 @@ export default function RootLayout({
             survives client-side navigation between Home and Resume. */}
         <Alfred />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
